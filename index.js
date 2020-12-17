8d162c819225ef2814a4ffb016686829f0ea2336
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-12-17 12:35:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame().
      */
+     
 if(checkCollision)
     /**
      * Otherwise, if the rock hasn't reached the bottom of
